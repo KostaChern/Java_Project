afc52ff560b423b813ff39e5e63cc7f4bd45057d
Konstantin_Chern <codahl@yandex.ru>
2023-06-16 12:42:46 +0300

    Task2 resolved

--- HW/HW_Sem2/src/App.java
+++ HW/HW_Sem2/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        task1.main(args);
+         task1.main(args);
+         task2.main(args);
     }
 }
--- HW/HW_Sem2/src/task1.java
+++ HW/HW_Sem2/src/task1.java
@@ -1,28 +1,51 @@
+
 // Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, 
 // используя StringBuilder. Данные для фильтрации приведены ниже в виде json строки.
 // Если значение null, то параметр не должен попадать в запрос.
 // Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+import java.io.*;
+
 public class task1 {
+
     public static void main(String[] args) throws Exception {
-      String [] list = ReadLineFromFile("dataForSelect.txt"); 
-      System.out.println(list[0]); 
-      StringBuilder resultSelect = LineInList(list[0]);
-      System.out.println(resultSelect);
+        String[] list = ReadLineFromFile("dataForSelect.txt");
+        System.out.println(list[0]);
+        StringBuilder resultSelect = LineInList(list[0]);
+        System.out.println(resultSelect);
     }
-    private static String[] ReadLineFromFile(String string) {
-                return null;
-}
-public static StringBuilder LineInList(String line) {
+                           //считывание строк из файла
+    public static String[] ReadLineFromFile(String pathway) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(pathway));
+        String str;
+        int size = 0;
+        while ((str = br.readLine()) != null) {
+            size += 1;
+        }
+        br.close();
+        String[] listData = new String[size];
+
+        int i = 0;
+        BufferedReader br1 = new BufferedReader(new FileReader(pathway));
+        while ((str = br1.readLine()) != null) {
+            listData[i] = str;
+            i += 1;
+        }
+        br1.close();
+        return listData;
+
+    }
+
+    public static StringBuilder LineInList(String line) {
         String line1 = line.replace("{", "");
         String line2 = line1.replace("}", "");
         String line3 = line2.replaceAll("\"", "");
         System.out.println(line3);
         StringBuilder result = new StringBuilder("select * from students where ");
 
-        String [] arrayData = line3.split(", ");
-        for (int i =0; i < arrayData.length; i++) {
+        String[] arrayData = line3.split(", ");
+        for (int i = 0; i < arrayData.length; i++) {
             String[] arrData = arrayData[i].split(":");
-            if(arrData[1].equals("null") == false) {
+            if (arrData[1].equals("null") == false) {
                 if (i != 0) {
                     result.append(", ");
                     result.append(arrData[0]);
@@ -34,7 +57,7 @@ public static StringBuilder LineInList(String line) {
                     result.append(arrData[1]);
                 }
             }
-            
+
         }
         return result;
     }
