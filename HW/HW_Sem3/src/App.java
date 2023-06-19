6ee5cb2f7cd0a04f7d29ddafff0674d9ca8c90cc
Konstantin_Chern <codahl@yandex.ru>
2023-06-19 14:20:53 +0300

    Sjlved Task2

--- HW/HW_Sem3/src/App.java
+++ HW/HW_Sem3/src/App.java
@@ -2,18 +2,20 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-       
+    
+        // GoTask1();
+        Task2.DelEvenNumbers(args);
+    }
+    private static void GoTask1() {
         Scanner input = new Scanner(System.in);
-        System.out.print("Введите элементы массива через пробел: ");
+        System.out.print("Заполните массив через пробел: ");
         String rawInput = input.nextLine();
         String[] array = rawInput.split(" ");
         int[] original = new int[array.length];
         for (int i = 0; i < original.length; i++)
             original[i] = (Integer.parseInt(array[i]));
-        Task1.print(original, "Исходный массив:");
+        Task1.print(original, "Начальный массив:");
         Task1.Metod(original);
         Task1.print(original, "Отсортированный массив:");
-        
-
     }
 }
