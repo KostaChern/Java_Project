dd486679e1fbfa55b39567fbbaad4f319a961747
Konstantin_Chern <codahl@yandex.ru>
2023-06-23 00:14:36 +0300

    Remined questions about the first task

--- HW/HW_Sem4/src/App.java
+++ HW/HW_Sem4/src/App.java
@@ -1,7 +1,11 @@
+import Collection.LinkListSolution;
+
 public class App {
     public static void main(String[] args) {
         // Task1.revers_arr(args);
-        Task2.workWithLinkedList(args);
+        // Task2.workWithLinkedList(args);
+        LinkListSolution.run();
+
 
 
     }
