6d85f1d1a5de607b0c80c4efb46e40ee63efc13c
Konstantin_Chern <codahl@yandex.ru>
2023-06-26 12:48:56 +0300

    Begin solved Task1

--- HW/HW_Sem4/src/App.java
+++ HW/HW_Sem4/src/App.java
@@ -2,9 +2,9 @@ import Collection.LinkListSolution;
 
 public class App {
     public static void main(String[] args) {
-        // Task1.revers_arr(args);
+        Task1.revers_arr(args);
         // Task2.workWithLinkedList(args);
-        LinkListSolution.run();
+        // LinkListSolution.run();
 
 
 
---
+++ HW/HW_Sem5/src/PhoneBook/Task1.java
@@ -0,0 +1,54 @@
+package PhoneBook;
+
+import java.util.*;
+
+// Задание
+// 1)Реализуйте структуру телефонной книги с помощью HashMap. Программа также должна учитывать, 
+// что во входной структуре будут повторяющиеся имена с разными телефонами, их необходимо считать, 
+// как одного человека с разными телефонами. Вывод должен быть отсортирован по убыванию числа телефонов.
+// (можно выводить без сортировки, но обязательно в отдельном методе)
+
+public class Task1 {
+
+                // Метод sortedPrint() сортирует и распечатывает данные по абонентам
+    static void sortedPrint(Map<String, ArrayList> map) {
+
+        // Получаем набор всех ключей abon
+        Set<String> keySet = map.keySet();
+
+        // Находим минимальное и максимальное значение
+        int maxCount = 0;
+        int minCount = 1_000_000;
+        
+        for (var item : map.entrySet()) {
+            if (maxCount < item.getValue().size())
+                maxCount = item.getValue().size();
+            if (minCount > item.getValue().size())
+                minCount = item.getValue().size();
+            
+        }
+        // Формируем стек, начиная с минимального количества номеров 
+        Stack<String> st = new Stack<>();
+        int num = minCount;
+        while (num <= maxCount) {
+            // System.out.println(map);
+            for (var item : map.entrySet()) {
+                if (item.getValue().size() == num) {
+                    st.push(item.getKey());
+                }  
+            }
+            num += 1;
+        }
+        // Распечатываем пары в порядке ключей, находящихся в стеке
+        String name;
+        for (int i = 0; i < keySet.size(); i++) {
+            name = st.pop();
+            for (var item : map.entrySet()) {
+                if (name == item.getKey()) {
+                    System.out.printf("%8s: ", item.getKey());
+                    System.out.println(item.getValue());
+                }
+            }
+        }
+        System.out.println();
+}
